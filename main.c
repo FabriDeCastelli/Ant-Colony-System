8a6418052356d1d11b59473b3f5fb3fedd0243ed
Fabrizio De Castelli <105369650+FabriDeCastelli@users.noreply.github.com>
2023-05-23 23:27:00 +0200

    Added documentation.

--- main.c
+++ main.c
@@ -1,13 +1,27 @@
 #include "support.h"
 #define NUM_FILES 10
 #define TIME_LIMIT 180
+
+// Problem Hyperparameters
 #define ANTS_NUMBER 10
 #define alpha 0.1
 #define rho 0.1
 #define beta 2.0
 #define q0 0.98
 
+/**
+ * Virtually remove the element of a given position.
+ * Does not deallocate memory.
+ * 
+ * @param array The array to be modified.
+ * @param position The position of the element to be removed.
+ * @param length The length of the array.
+ * 
+ * @return The modified array.
+ */
 int *shift(int *array, int position, int *length) {
+
+  assert(position < *length && position >= 0);
 
   array[position] = array[*length - 1];
   array[*length - 1] = -1;
@@ -16,12 +30,21 @@ int *shift(int *array, int position, int *length) {
   return array;
 }
 
+/**
+ * Randomly positions ants in cities.
+ * 
+ * @param ants_number The number of ants.
+ * @param N_CITIES The number of cities of our problem.
+ * 
+ * @return The array of ants.
+ */
 ant *position_ants(int ants_number, int N_CITIES) {
-  // printf("city number:%d\n", N_CITIES);
-  ant *ants = malloc(ANTS_NUMBER * sizeof(ant));
-  if (ants == NULL)
-    stop("Position_ants malloc 1 failed", HERE);
 
+
+  ant *ants = malloc(ANTS_NUMBER * sizeof(ant));
+  if (ants == NULL) {
+    stop("Position_ants malloc 1 failed", HERE);
+  }
   for (int i = 0; i < ants_number; i++) {
     int r = rand() % N_CITIES;
     ants[i].starting_city = r;
@@ -38,14 +61,20 @@ ant *position_ants(int ants_number, int N_CITIES) {
       }
     }
     ants[i].tour = malloc((N_CITIES + 1) * sizeof(int));
-    if (ants[i].tour == NULL)
+    if (ants[i].tour == NULL) {
       stop("Position_ants tour malloc 2 failed", HERE);
+    }
     ants[i].tour[0] = ants[i].starting_city;
   }
 
   return ants;
 }
 
+/**
+ * Frees the memory allocated for the ants.
+ * 
+ * @param ants The array of ants.
+ */
 void kill_ants(ant *ants) {
   for(int i = 0; i < ANTS_NUMBER; i++) {
     free(ants[i].tour);
@@ -54,6 +83,15 @@ void kill_ants(ant *ants) {
   free(ants);
 }
 
+/**
+ * Exploits the pheromone trail in the current solution.
+ * 
+ * @param N_CITIES The number of cities of our problem.
+ * @param adjacence The matrix of edges.
+ * @param ant The ant that is currently moving.
+ * 
+ * @return The index of the next city to be visited.
+*/
 int exploit(int N_CITIES, edge adjacence[][N_CITIES], ant ant) {
   
   int next_city = -1;
@@ -71,6 +109,15 @@ int exploit(int N_CITIES, edge adjacence[][N_CITIES], ant ant) {
   return next_city;
 }
 
+/**
+ * Explores the solution space.
+ * 
+ * @param N_CITIES The number of cities of our problem.
+ * @param adjacence The matrix of edges.
+ * @param ant The ant that is currently moving.
+ * 
+ * @return The index of the next city to be visited. 
+*/
 int explore(int N_CITIES, edge adjacence[][N_CITIES], ant ant) {
   
   int next_city = -1;
@@ -97,22 +144,40 @@ int explore(int N_CITIES, edge adjacence[][N_CITIES], ant ant) {
   return next_city;
 }
 
+/**
+ * A given ant performs exploitation with probability q0 and exploration with probability 1 - q0.
+ * 
+ * @param N_CITIES The number of cities of our problem.
+ * @param adjacence The matrix of edges.
+ * @param ant The ant that is currently moving.
+ * 
+ * @return The index of the next city to be visited.
+*/
 int state_transition_rule(int N_CITIES, edge adjacence[][N_CITIES], ant ant) {
   
   double q = (double) rand() / (double) RAND_MAX;
-  if (q <= q0) 
+  if (q <= q0) {
     return exploit(N_CITIES, adjacence, ant);
+  }
   return explore(N_CITIES, adjacence, ant);
   
 }
 
 int tour_cost(ant ant, int N_CITIES, edge adjacence[][N_CITIES]) {
   int length = 0;
-  for(int i = 0; i < N_CITIES; i++) 
+  for(int i = 0; i < N_CITIES; i++) {
     length = length + adjacence[ant.tour[i]][ant.tour[i+1]].cost;
+  }
   return length;
 } 
 
+/**
+ * Swaps the cities i and j in the tour, as well as all cities in between.
+ * 
+ * @param tour The tour to be modified.
+ * @param i The first city to be swapped.
+ * @param j The second city to be swapped.
+*/
 void swap(int *tour, int i, int j) {
   while(i < j) {
     int temp = tour[i];
@@ -123,6 +188,13 @@ void swap(int *tour, int i, int j) {
   }
 }
 
+/**
+ * Performs the 2-opt local search algorithm on the given tour.
+ * 
+ * @param tour The tour to be optimized.
+ * @param N_CITIES The number of cities of our problem.
+ * @param adj The matrix of edges.
+*/
 void two_opt(int *tour, int N_CITIES, edge adj[][N_CITIES]) {
   bool improvement = true;
   while(improvement) {
@@ -143,12 +215,20 @@ void two_opt(int *tour, int N_CITIES, edge adj[][N_CITIES]) {
   }
 }
 
+/**
+ * Solves the problem using the Ant Colony Optimization algorithm.
+ * 
+ * @param cities The array of cities.
+ * @param N_CITIES The number of cities of our problem.
+ * 
+ * @return The cost of the best tour found.
+*/
 int solve(city *cities, int N_CITIES) {
 
   int NN_sol = NN(cities, N_CITIES);
   double tau0 = 1.0 / (NN_sol * N_CITIES);
 
-  // initialization of pheromone and distance matrix
+  // Pheromone and distance matrix initialization
   edge adjacence[N_CITIES][N_CITIES];
   edge data;
   data.pheromone = tau0;
@@ -159,7 +239,7 @@ int solve(city *cities, int N_CITIES) {
     }
   }
 
-  // variable to save best tour cost found so far
+  // Saves best tour cost found so far
   int best_found = INT_MAX;
 
   time_t start = time(NULL);
@@ -168,21 +248,23 @@ int solve(city *cities, int N_CITIES) {
     ant *ants = position_ants(ANTS_NUMBER, N_CITIES);
     for (int i = 0; i < N_CITIES; i++) {
       
-      if (i < N_CITIES - 1) { // ants choose next city
+      if (i < N_CITIES - 1) { 
+        // Ants choose next city
         for (int j = 0; j < ANTS_NUMBER; j++) {
           int position = state_transition_rule(N_CITIES, adjacence, ants[j]);
           ants[j].next_city = ants[j].not_visited[position];
           ants[j].tour[i+1] = ants[j].next_city;
           ants[j].not_visited = shift(ants[j].not_visited, position, &ants[j].nv_len);
         }    
-      } else { // ants go back to their initial city      
+      } else { 
+        // Ants go back to their initial city      
         for (int j = 0; j < ANTS_NUMBER; j++) {
           ants[j].tour[i+1] = ants[j].starting_city;
           ants[j].next_city = ants[j].starting_city;
         }       
       }
 
-      //local update
+      // Local update
       for (int j = 0; j < ANTS_NUMBER; j++) {
         double pheromone = adjacence[ants[j].current_city][ants[j].next_city].pheromone;
         pheromone = (1 - rho) * pheromone + rho * tau0;
@@ -192,7 +274,7 @@ int solve(city *cities, int N_CITIES) {
 
     }
 
-    //  compute best tour for best ant 
+    // Compute best tour for best ant 
     int tours[ANTS_NUMBER];
     int best_tour = INT_MAX;
     int best_ant = -1;
@@ -205,7 +287,7 @@ int solve(city *cities, int N_CITIES) {
       }
     }
 
-    // global update
+    // Global update
     for(int i = 0; i < N_CITIES; i++) {
       int from = ants[best_ant].tour[i];
       int to = ants[best_ant].tour[i+1];
@@ -225,6 +307,9 @@ int solve(city *cities, int N_CITIES) {
   
 }
 
+/**
+ * Main function.
+ */
 int main(int argc, char *argv[]) {
 
   srand(time(NULL));
